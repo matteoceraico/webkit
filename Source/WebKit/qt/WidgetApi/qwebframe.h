7a8802b3dbd9304af9131e8aa730b8e8b24f74d5
Cerri <maceraico@gmail.com>
2016-10-26 17:33:28 +0200

    Provided ability to set the url for evaluated javascripts

--- Source/WebKit/qt/WidgetApi/qwebframe.h
+++ Source/WebKit/qt/WidgetApi/qwebframe.h
@@ -209,7 +209,7 @@ public:
     QWebFrameAdapter* handle() const;
 
 public Q_SLOTS:
-    QVariant evaluateJavaScript(const QString& scriptSource);
+    QVariant evaluateJavaScript(const QString& scriptSource, const QUrl& location = QUrl());
 #ifndef QT_NO_PRINTER
     void print(QPrinter *printer) const;
 #endif
